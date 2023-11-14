ea1ca7ad119ea7a64f1637fdd4c31ada9b6c8dbd
Romanenko Roman <Romanenko.roman.1983@yandex.ru>
2023-11-14 23:18:22 +0300

    Add new file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import randint
 def attack(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
-                f' {5 + randint(3, 5)}')
+                f'{5 + randint(3, 5)}')
     if char_class == 'mage':
         return (f'{char_name} нанёс урон противнику равный '
                 f' {5 + randint(5, 10)}')
